9e26549a853c7bd266f015d7f47d8432f8405786
modou2-DIA <101325666+modou2-DIA@users.noreply.github.com>
2025-02-26 23:05:37 +0100

    Ajout des composants pour strcuturer l application

---
+++ src/app/modules/schedule/schedule-management/schedule-management.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-schedule-management',
+  imports: [],
+  templateUrl: './schedule-management.component.html',
+  styleUrl: './schedule-management.component.css'
+})
+export class ScheduleManagementComponent {
+
+}
